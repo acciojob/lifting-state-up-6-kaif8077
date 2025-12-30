b1f2175f3e01580dea5e4530660c9bbc97c21d59
Mohammad Kaif <mohdkaif90275@gmail.com>
2025-12-30 13:16:15 +0530

    kaif

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,29 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import TodoList from "./TodoList";
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    { id: 1, text: "learn react", completed: false },
+    { id: 2, text: "python", completed: false },
+    { id: 3, text: "javascript", completed: false }
+  ]);
+
+  function handleComplete(id) {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === id
+        ? { ...todo, completed: true }
+        : todo
+    );
+
+    setTodos(updatedTodos);
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Parent Component</h1>
+      <TodoList todos={todos} handleComplete={handleComplete} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
